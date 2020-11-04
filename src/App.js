b945692f755b26127c12e4e3215f225a2ddb6be8
Andre Putera Pratama <andreputerap@gmail.com>
2020-11-05 04:12:41 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,89 +1,56 @@
-import React, { Component } from 'react';
-import { withRouter, Route, Switch } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { withRouter, useLocation } from 'react-router-dom';
 
 // REDUX //
 import {connect} from 'react-redux';
 import {keepLogin} from './4. redux/actions';
-// REDUX //
 
 // STYLES //
 import './3. assets/styles/App.css';
 import './3. assets/styles/pages.css';
 import './3. assets/styles/components.css';
-// STYLES //
 
 // TOAST //
 import 'react-toastify/dist/ReactToastify.min.css';
 import { ToastContainer } from 'react-toastify';
-// TOAST //
 
 // COMPONENTS
+import Routes from './Routes';
 import Navbar from './1. components/Navbar';
 import Footer from './1. components/Footer';
 import LoadingScreen from './2. pages/LoadingScreen';
-// COMPONENTS
 
-// PAGES //
-import Home from './2. pages/Home';
-import Login from './2. pages/Auth/Login';
-import Register from './2. pages/Auth/Register';
-import EmailVerifying from './2. pages/EmailVerifying';
-import EmailVerified from './2. pages/EmailVerified';
-import BuatCampaign from './2. pages/Campaign/buatCampaign';
-import SemuaCampaign from './2. pages/Campaign/semuaCampaign';
-import DetailCampaign from './2. pages/Campaign/detailCampaign';
-import BuatPolling from './2. pages/Polling/buatPolling';
-import SemuaPolling from './2. pages/Polling/semuaPolling';
-import Dashboard from './2. pages/Dashboard/DashboardRoute';
-import PageNotFound from './2. pages/404';
-// PAGES //
+const App = (props) => {
+  // VARIABLES
+  const loc = useLocation()
 
-
-class App extends Component {
-  // LIFECYCLE //
-  componentDidMount() {
+  // LIFECYCLE
+  useEffect(() => {
     let user = localStorage.getItem('token')
-    this.props.keepLogin(user)
-  }
-  // LIFECYCLE //
+    props.keepLogin(user)
+  }, [])
 
+  // RENDER
+  return !props.loadChecker
+  ? <LoadingScreen />
+  : (
+    <div>
+      <ToastContainer
+        autoClose={4000}
+        style={{zIndex:9999}}
+      />
 
-  // MAIN RENDER
-  render() {
-    if (!this.props.loadChecker) {
-      return <LoadingScreen />
-    }
+      {
+        loc.pathname === "/login" || loc.pathname === "/register"
+        ? null
+        : <Navbar/>
+      }
 
-    return (
-      <div>
-        {/* TOAST */}
-        <ToastContainer
-          autoClose={4000}
-          style={{zIndex:9999}}
-        />
-        {/* TOAST */}
+      <Routes />
 
-        <Navbar/>
-
-        <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/login' exact component={Login} />
-          <Route path='/register' exact component={Register} />
-          <Route path='/EmailVerifying' exact component={EmailVerifying} />
-          <Route path='/EmailVerified' exact component={EmailVerified} />
-          <Route path='/campaign' exact component={SemuaCampaign} />
-          <Route path='/campaign/:id' exact component={DetailCampaign} />
-          <Route path='/buat-campaign' exact component={BuatCampaign} />
-          <Route path='/polling' exact component={SemuaPolling} />
-          <Route path='/buat-polling' exact component={BuatPolling} />
-          <Route path='/user' component={Dashboard} />
-          <Route path='*' component={PageNotFound} />
-        </Switch>
-
-        <Footer/>
-      </div>
-    );
-  }
+      <Footer/>
+    </div>
+  )
 }
 
 const mapStateToProps = ({userData}) => {
