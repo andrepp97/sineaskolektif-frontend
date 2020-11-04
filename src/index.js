8100cdb55d0dac0ad2f725027990a3417f569d8b
Andre Putera Pratama <andreputerap@gmail.com>
2020-11-05 04:12:45 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
-
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 // MDBREACT //
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
-// MDBREACT //
 
 // REDUX //
 import ReduxThunk from 'redux-thunk'
@@ -32,8 +29,3 @@ ReactDOM.render(
     </Provider>,
     document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
