af5afbc9880880c9896fc41a05c1a60959f6b3d4
Andre Putera Pratama <andreputerap@gmail.com>
2020-11-05 04:10:04 +0700

    Update Footer.jsx

--- src/1. components/Footer.jsx
+++ src/1. components/Footer.jsx
@@ -1,28 +1,36 @@
 import React from "react"
+import Logo from '../3. assets/img/Logo_white.png';
 import { MDBCol, MDBContainer, MDBRow, MDBFooter, MDBIcon } from "mdbreact"
 
+const date = new Date()
 
 const Footer = () => {
     return (
-        <MDBFooter className="font-small bg-dark pt-5">
+        <MDBFooter className="sineaskolektif-footer font-small pt-5">
             <MDBContainer className="text-center text-md-left">
                 <MDBRow>
                     <MDBCol md="6" className='my-3'>
-                        <h5 className="title">ABOUT SINEAS KOLEKTIF</h5>
+                        <h5 className="title">TENTANG</h5>
                         <ul className='list-group'>
                             <li className="list-unstyled">
                                 <a href="#!" className='grey-text'>FAQ</a>
                             </li>
                             <li className="list-unstyled">
-                                <a href="#!" className='grey-text'>Contact Us</a>
+                                <a href="#!" className='grey-text'>Kontak</a>
                             </li>
                             <li className="list-unstyled">
-                                <a href="#!" className='grey-text'>Terms and Conditions</a>
+                                <a href="#!" className='grey-text'>Tentang Kami</a>
+                            </li>
+                            <li className="list-unstyled">
+                                <a href="#!" className='grey-text'>Kebijakan Privasi</a>
+                            </li>
+                            <li className="list-unstyled">
+                                <a href="#!" className='grey-text'>Syarat dan Ketentuan</a>
                             </li>
                         </ul>
                     </MDBCol>
                     <MDBCol md="6" className='my-3'>
-                        <h5 className="title">FOLLOW US</h5>
+                        <h5 className="title">IKUTI KAMI</h5>
                         <div className='list-inline'>
                             <li className='list-inline-item'>
                                 <a href="https://www.instagram.com" target='_blank' rel="noopener noreferrer">
@@ -36,7 +44,12 @@ const Footer = () => {
                             </li>
                             <li className="list-inline-item ml-2">
                                 <a href="https://github.com" target='_blank' rel="noopener noreferrer">
-                                    <MDBIcon fab icon="github" size="2x" />
+                                    <MDBIcon fab icon="twitter" size="2x" />
+                                </a>
+                            </li>
+                            <li className="list-inline-item ml-2">
+                                <a href="https://github.com" target='_blank' rel="noopener noreferrer">
+                                    <MDBIcon fab icon="linkedin" size="2x" />
                                 </a>
                             </li>
                         </div>
@@ -44,10 +57,12 @@ const Footer = () => {
                 </MDBRow>
             </MDBContainer>
 
-            <div className="text-center py-5">
-                <MDBContainer className='border-top border-bottom w-100 py-3'>
-                    &copy; 2020 Copyright by
-                    <span className="opacity-90 ml-1">SINEAS KOLEKTIF</span>
+            <div className="py-5">
+                <MDBContainer className='border-top w-100 pt-4'>
+                    <img src={Logo} alt="Sineas Kolektif" height={40} />
+                    <p className="opacity-90 mt-3">
+                        &copy; {date.getFullYear()} Sineas Kolektif. All Rights Reserved.
+                    </p>
                 </MDBContainer>
             </div>
         </MDBFooter>
